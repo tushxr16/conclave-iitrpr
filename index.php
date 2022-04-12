97e799f759a56eb56e017e25fef861da55aa2766
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-12 10:04:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include("header.php");
+?>
+
+<div class="container-fluid">
+    <div class="row justify-content-evenly stars-bg">
+        <div class="col-md-5 cent w3-animate-left">
+            <div class="heading">
+                Parley, Pioneer And Prosper
+            </div>
+            <div class="sub-heading">
+                Leadership Conclave 2022: A step towards building a forum to delimitate the Inter-IIT boundaries
+            </div>
+            <div class="dates-layout">
+                DATES: 13-15 May '22
+            </div>
+        </div>
+        <div class="col-md-5 w3-animate-right">
+            <img src="./images/meeting.png" class="img-resp"></img>
+        </div>
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
---
+++ prelude.php
@@ -0,0 +1,25 @@
+<?php
+include("header.php");
+?>
+<div class="container-fluid">
+    <div class="row justify-content-evenly">
+        <div class="col-md-5 w3-animate-left">
+            <img src="./images/prelude-1.png" class="img-resp"></img>
+        </div>
+        <div class="col-md-5 cent w3-animate-right">
+            <div class="heading">
+                PRELUDE
+            </div>
+            <div class="prelude-content">
+                The name of IITs is synonymous with excellence and innovation. The exchange of ideas and knowledge here, among the keen to learn individuals and the experienced ones like - postgraduates and professors, blossoms into solutions that lead to the betterment of the society as a whole.
+                <br><br>The leadership summit aims to shed light on the challenges students face due to the rapidly changing socio-educational frameworks and put forward resolutions that would help the students excel in the face of this colossal paradigm shift. By engaging in discussion and brainstorming reforms, we can keep up with the trends and lead the other institutes to do the same.
+                <br><br>As student leaders, we aim to formulate solutions that can be implemented across all institutions with the help of our ministry since it is our duty, as one of the premier institutes of the nation, to benefit every individual that faces the same challenge.
+                <br><br>The objective of this three-day leadership summit is to bridge the gap between the UG and the PG students in our institutes. We intend to put forward the concerns of the students to the Ministry of Education to include the highest authorities in the thick of things
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
