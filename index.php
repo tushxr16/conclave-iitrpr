b5da00d2acd468ef3fcbdd7614e8f2f54288f95a
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-04-18 15:19:38 +0530

    404

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
     <title>Leadership Summit 2022</title>
     <link rel="stylesheet" href="styles/home.css" />
     <link rel="stylesheet" href="styles/animation.css" />
-    <link rel="stylesheet" href="styles/team.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
